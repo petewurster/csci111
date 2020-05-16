3e3e3e8a1ea51c6ed2da0d26fe8f137a7a9cfff4
petewurster <pwurste1@student.ccp.edu>
2020-05-16 15:44:41 -0400

    refactor testing

---
+++ personal_exercises/aa.py
@@ -0,0 +1,8 @@
+#aa.py;
+#depencancies: lklklk
+#last edit: 2020-05-16, by pWurster;
+#
+#
+
+import lklklk
+uyuyuy
--- personal_exercises/doxme.py
+++ personal_exercises/doxme.py
@@ -4,6 +4,7 @@
 #desc: adds documentation to files as per prof Herbert's spec
 #pass any number of files to update in bulk
 
+
 import sys
 import re
 from datetime import date
@@ -13,21 +14,28 @@ AUTHOR = 'pWurster'
 def read(file):
     with open(file, 'r') as original:
         lines = original.readlines()
-        mods = ''
-        for line in lines:
-#### add a func to determine language
-            if 'import' in line and line[0] != '#':
-                mod = re.split("import", line)[1].rstrip()
-#### regex needs streamlining
-                if re.search("[]:!?=+()[\-{}]+", mod): pass
-                else: mods += f'{mod},'
-            mods = mods[:-1] + ''
-        lines.insert(0, f'#{file};\n#depencancies:{mods if mods else " none;"}\n#last edit: {date.today()}, by {AUTHOR};\n#desc:\n')	
+        modules = ''
+        if detLang(file) == 'python': modules = applyRegexForPy(lines)
+        lines.insert(0, f'#{file};\n#depencancies:{modules if modules else " none;"}\n#last edit: {date.today()}, by {AUTHOR};\n#\n#\n\n')	
         return lines
 
-def write(file, data):
-    with open(file, 'w') as documented:
-        for line in data: documented.write(line)
+def write(file, newData):
+    with open(file, 'w') as doxedFile:
+        for line in newData: doxedFile.write(line)
+
+def detLang(file):
+    if re.match('.*\.py', file): return 'python'
+    return 'unknown'
+
+def applyRegexForPy(lines):
+    mods = ''
+    for line in lines:
+        if 'import' in line and line[0] != '#':
+            mod = re.split("import", line)[1].rstrip()
+            if re.search("[]:!?=+()[\-{}]+", mod): pass
+            else: mods += f'{mod},'
+    return mods[:-1] + ''
+
 
 def main():
     for file in sys.argv:
@@ -36,4 +44,15 @@ def main():
             try: write(file, read(file))
             except: print(f'error processing file: {file}')
 
+
 if __name__ == '__main__': main()
+
+
+""" to do:
+generalize line looping;
+add regex for other lang module recognition (node, mjs, php, java, swift)
+improve comment recognition (EOL comments, multi-line comments)
+improve existing regex (distinguish 'import' && 'from .* import')
+
+"""
+
