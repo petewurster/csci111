294fd575de26f6f79fdfb7063293ad5b6eab2ef9
petewurster <pwurste1@student.ccp.edu>
2020-06-02 18:55:24 -0400

    alt syntax

--- class_labs/pw_lab06_basketballDict.py
+++ class_labs/pw_lab06_basketballDict.py
@@ -1,12 +1,18 @@
 #pw_lab06_basketballDict.py;
 #depencancies: none;
-#last edit: 2020-06-01, by pWurster;
+#last edit: 2020-06-02, by pWurster;
 #demonstrate basic dictionary functions via assignment, iteration,
 #and dict[key] access to display some sports results
 
 #init vars
-playerPoints: dict = dict(Bolden = 1, Butler = 11, Embiid = 31, Ennis = 7, Harris = 24,
-                          Marjanovic = 4, McConnell = 2, Reddick = 9, Scott = 8, Simmons = 15)
+playerPoints: dict = dict(Bolden = 1, Butler = 11, Embiid = 31, Ennis = 7,
+                          Harris = 24, Marjanovic = 4, McConnell = 2,
+                          Reddick = 9, Scott = 8, Simmons = 15)
+#alternate syntax to declare dict as a literal
+#playerPoints: dict = {'Bolden' : 1, 'Butler' : 11, 'Embiid' : 31, 'Ennis' : 7,
+#                      'Harris' : 24, 'Marjanovic' : 4, 'McConnell' : 2,
+#                      'Reddick' : 9, 'Scott' : 8, 'Simmons' : 15}
+
 highestScoringPlayer:str = '' #init to empty string
 highScore:int = 0 #will be set in loop
 totalScore: int = 0 #accumulator
